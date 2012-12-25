4c7fae80bd529e9e10b698477cc1f1d0399a5ca7
Justin Duke <justinduke@Justins-MacBook-Air.local>
2012-12-24 22:13:52 -0500

    Remove requests dependency.

--- aww.py
+++ aww.py
@@ -1,28 +1,50 @@
 from random import randint
 from PIL import Image
-import requests
 import urllib2
 import StringIO
+import ast
+import sys
 
-# Grab the JSON of the r/aww subreddit, which displays
-# adorable images.
-reddit_json = requests.get("http://www.reddit.com/r/aww.json").json
-
-# By default, a subreddit displays the top 25 images;
-# we grab a random one of those
-random_index = randint(0, 24) 
-
-# Next, we navigate the json and grab the link (generally imgur)
-# to the adorable image in question.
-image_link = reddit_json['data']['children'][random_index]['data']['url']
-
-# Now, we open the link with imgur!
-img = urllib2.urlopen(image_link).read()
-
-# Make sure everything works nice;
-# if it does, show the image using PIL.
-try:
-	im = Image.open(StringIO.StringIO(img))
-	im.show()
-except Exception, e:
-	print e
+# This is where the magic happens.
+def grab_cute_image(username = None):
+	
+	# Grab the JSON of the r/aww subreddit, which displays
+	# adorable images -- and convert it to a dict.
+	cuteness_url = "http://www.reddit.com/r/aww.json"
+	opener = urllib2.build_opener()
+	
+	# if the user supplies a username, we add it to the UA.
+	if username:
+		opener.addheaders = [('User-agent', 'aww.py, invoked by /u/' + username)]
+	else:
+		opener.addheaders = [('User-agent', 'aww.py, a CLI for r/aww!')]
+		
+	json = opener.open(cuteness_url).read()
+	(true,false,null) = (True,False,None) # Booleans can be funky.
+	json = eval(json)
+	
+	# By default, a subreddit displays the top 25 images;
+	# we grab a random one of those
+	random_index = randint(0, 24) 
+	
+	# Next, we navigate the json and grab the link (generally imgur)
+	# to the adorable image in question.
+	image_link = json['data']['children'][random_index]['data']['url']
+	
+	# Now, we open the link with imgur!
+	img = urllib2.urlopen(image_link).read()
+	
+	# Make sure everything works nice;
+	# if it does, show the image using PIL.
+	try:
+		im = Image.open(StringIO.StringIO(img))
+		im.show()
+	
+	# What's a silly side project without unncessary recursion?
+	except Exception, e:
+		grab_cute_image()
+		
+if len(sys.argv) < 2:
+	grab_cute_image()
+else:
+	grab_cute_image(sys.argv[1])
