5e3dd8807f0ffd213c4654bf3cbc0ed47c8b006a
Justin Duke <justinduke@Justins-MacBook-Air.local>
2012-12-27 16:40:30 -0500

    Handling urls that aren't strictly images

--- aww.py
+++ aww.py
@@ -4,12 +4,45 @@ import json
 import urllib2
 import StringIO
 import sys
+import re
 
 # TODO: Error handling.
 # Works if Reddit's permalink is an absolute image;
 # will require some finagling down the line to make it work
 # with the inevitable edge cases.
 
+# given a url where an image may contain,
+# return a bitstream of the image
+def load_image(url):
+    
+    image_suffixes = ['.png', '.jpg', '.gif']
+    
+    # base case: the url is an image
+    if url[-4:] in image_suffixes:    
+        img = urllib2.urlopen(url).read()
+    
+    # if linking to the imgur page, append '.jpg'
+    # to get the image itself
+    elif 'imgur' in url:
+        img = urllib2.urlopen(url + '.jpg').read()
+        
+    # if it's a random page, return the first image
+    # found on the page
+    else:
+        source_code = urllib2.urlopen(url).read()
+        
+        # Simple regex; assumes valid syntax
+        first_linked_image = re.search(r"<img src=[\'|\"](.+)[\'|\"](.+)/?>", source_code).groups()[0]
+        
+        # that could have grabbed attributes after the src, so
+        # we clean it up
+        truncate_index = first_linked_image.find(" ")
+        first_linked_image = first_linked_image[:truncate_index - 1]
+        img = urllib2.urlopen(first_linked_image).read()
+        
+    return img
+        
+        
 
 # This is where the magic happens.
 def grab_cute_image(username=None, trials=3):
@@ -41,18 +74,18 @@ def grab_cute_image(username=None, trials=3):
     # Next, we navigate the json and grab the link (generally imgur)
     # to the adorable image in question.
     image_link = j['data']['children'][random_index]['data']['url']
-
+    
+    image = load_image(image_link)
+    
     # Now, we open the link with imgur!
-    img = urllib2.urlopen(image_link).read()
-
     # Make sure everything works nice;
     # if it does, return our image
     try:
-        im = Image.open(StringIO.StringIO(img))
+        im = Image.open(StringIO.StringIO(image))
         return im
 
         # What's a silly side project without unncessary recursion?
-    except Exception:
+    except Exception as e:
         return grab_cute_image(username, trials - 1)
 
 
